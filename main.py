44928be0a4e6c2a9fe724572b02cbffcd1b75b0b
til3000 <129494050+til3000@users.noreply.github.com>
2024-04-11 22:06:55 +0900

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-		numToIndex = {}
+	numToIndex = {}
     for index, num in enumerate(nums):
-        comp = target - num
+		comp = target - num
         if comp in numToIndex:
             return [numToIndex[comp], index]
         numToIndex[num] = index
-    return []
+return []
